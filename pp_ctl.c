7705f46a6fc876532e0b96d07e5e284e65f95664
Reini Urban <rurban@cpanel.net>
2016-07-04 12:49:43 +0200

    unexec: add sources from emacs-24.5, probe for it
    not yet enabled, since we need to change the sources a bit.

--- pp_ctl.c
+++ pp_ctl.c
@@ -2810,7 +2810,6 @@ S_dofindlabel(pTHX_ OP *o, const char *label, STRLEN len, U32 flags, OP **opstac
     return 0;
 }
 
-
 static void
 S_check_op_type(pTHX_ OP * const o)
 {
@@ -2830,6 +2829,7 @@ S_check_op_type(pTHX_ OP * const o)
 }
 
 /* also used for: pp_dump() */
+/* To cvref or label. Any expr can be a a cvref or label. */
 
 PP(pp_goto)
 {
