0d8e1499ea74376c30b4a884d1ea093f208825f6
Reini Urban <rurban@cpan.org>
2017-06-10 11:31:38 +0200

    B 1.68_09: ffi support
    CvXFFI, CvFFLIB as new objects
    CVf_EXTERN flag

--- dist/Module-CoreList/lib/Module/CoreList.pm
+++ dist/Module-CoreList/lib/Module/CoreList.pm
@@ -17655,6 +17655,7 @@ our %delta :const = (
             'Module::CoreList'      => '5.20180913c',
             'Module::CoreList::Utils'=> '5.20180913c',
             'attributes'            => '1.15c',
+            'B'                     => '1.71_09',
             'hashiter'              => '0.02',
             'Errno'                 => '1.29_01',
             'Opcode'                => '1.40_03c',
