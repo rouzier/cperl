110fa7498ec751ce6dc28343107d7ca5da96053a
Reini Urban <rurban@cpan.org>
2017-08-28 12:33:13 +0200

    inline: add op_clone and cv_do_inline
    only op_clone_oplist needs to be functional at this point.
    moved cv_do_inline from rpeep to ck_subr without the op_next ptr chains.
    we only want to adjust the ops structurally, not in sequence.
    
    squashed from the inline branch from 2015, written at cPanel.

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1771,6 +1771,7 @@ use File::Glob qw(:case);
                 lib/filetest.{pm,t}
                 lib/h2ph.t
                 lib/h2xs.t
+                lib/inline.{pm,t}
                 lib/integer.{pm,t}
                 lib/less.{pm,t}
                 lib/locale.{pm,t}
